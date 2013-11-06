dc69d34f8d7383d809cb59b6c274c67d787682fa
guohailin <hazirguo@gmail.com>
2013-11-06 17:09:52 +0800

    add print memory representation

--- 2013-11-06-little_big_endian/alg1.c
+++ 2013-11-06-little_big_endian/alg1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+void show_mem(char *start, int n)
+{
+    int i;
+    for (i=0; i<n; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+
 int main()
 {
-    unsigned int i = 1;
+    unsigned int i = 0x01234567;
+    show_mem((char *)&i, 4);
     char *c = (char *)&i;
-    if (*c)
-        printf("Little Endian\n");
-    else
+    if (*c == 1)
         printf("Big Endian\n");
+    else
+        printf("Little Endian\n");
 
     return 0;
 }
